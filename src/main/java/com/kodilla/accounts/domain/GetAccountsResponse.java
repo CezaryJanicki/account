9b39997287be55967c8e3421c7692b45aad84b1a
CezaryJanicki <cezary.janicki@gmail.com>
2021-04-27 14:28:37 +0200

    changes to GetAccountsResponse:

--- src/main/java/com/kodilla/accounts/domain/GetAccountsResponse.java
+++ src/main/java/com/kodilla/accounts/domain/GetAccountsResponse.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -12,6 +13,6 @@ import java.util.List;
 @NoArgsConstructor
 public class GetAccountsResponse {
 
-    private List<AccountDto> accounts;
+    private List<AccountDto> accounts = new ArrayList<>();
 
 }
